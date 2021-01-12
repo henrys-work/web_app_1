b9d27af0ddbb120b9707dd46a63abe7d7d4fcf0f
Henry.Morrin <henry.morrin@iii.com>
2021-01-12 22:59:33 +0000

    Added requirements file for pip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app=Flask(__name__)
 
@@ -6,5 +7,12 @@ app=Flask(__name__)
 def home():
     return "Hello World"
 
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
